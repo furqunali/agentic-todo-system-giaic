cd2d7ad214b2d29a004ee0cbbba9282f67a8d857
Furqan Ali <furqanali2628@gmail.com>
2026-09-22 23:58:51 +0500

    api: define stable todo-domain public exports

--- todo_core/__init__.py
+++ todo_core/__init__.py
@@ -1,4 +1,19 @@
-from .domain import TodoItem
-from .validation import validate_title, validate_priority
+"""Public API for the reusable todo domain package."""
 
-__all__ = ["TodoItem", "validate_title", "validate_priority"]
+from .domain import TodoItem
+from .filters import filter_by_category, filter_by_priority
+from .prioritization import prioritize
+from .query import search
+from .validation import validate_priority, validate_title
+from .workload import workload_summary
+
+__all__ = [
+    "TodoItem",
+    "filter_by_category",
+    "filter_by_priority",
+    "prioritize",
+    "search",
+    "validate_priority",
+    "validate_title",
+    "workload_summary",
+]
